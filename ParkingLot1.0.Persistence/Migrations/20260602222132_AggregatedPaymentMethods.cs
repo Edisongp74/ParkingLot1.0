fedc7d652d8664e5c1125b80b9c9ae454eb6aa0b
Juan Esteban Vélez Tobón <estebanvelez93@gmail.com>
2026-06-02 19:24:49 -0500

    feat: corrección de gráfica del dashboard

---
+++ ParkingLot1.0.Persistence/Migrations/20260602222132_AggregatedPaymentMethods.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkingLot1._0.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AggregatedPaymentMethods : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MonthlyPassId",
+                table: "Payments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MonthlyPassId",
+                table: "Payments",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
